0cb2e3840e102d8d211189a387a6a35f205ad957
k0r0 <bmenonprayush@gmail.com>
2026-01-04 07:50:04 +0000

    update autocmd for wezterm Full screen

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,26 +1,36 @@
 require "nvchad.autocmds"
+local autocmd = vim.api.nvim_create_autocmd
 -- Restore last cursor position when reopening a file
 local last_cursor_group = vim.api.nvim_create_augroup("LastCursorGroup", {})
-vim.api.nvim_create_autocmd("BufReadPost", {
-	group = last_cursor_group,
-	callback = function()
-		local mark = vim.api.nvim_buf_get_mark(0, '"')
-		local lcount = vim.api.nvim_buf_line_count(0)
-		if mark[1] > 0 and mark[1] <= lcount then
-			pcall(vim.api.nvim_win_set_cursor, 0, mark)
-		end
-	end,
+autocmd("BufReadPost", {
+  group = last_cursor_group,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
 
 -- Highlight the yanked text for 200ms
 local highlight_yank_group = vim.api.nvim_create_augroup("HighlightYank", {})
-vim.api.nvim_create_autocmd("TextYankPost", {
-	group = highlight_yank_group,
-	pattern = "*",
-	callback = function()
-		vim.hl.on_yank({
-			higroup = "IncSearch",
-			timeout = 200,
-		})
-	end,
+autocmd("TextYankPost", {
+  group = highlight_yank_group,
+  pattern = "*",
+  callback = function()
+    vim.hl.on_yank {
+      higroup = "IncSearch",
+      timeout = 200,
+    }
+  end,
+})
+
+-- Remove padding and spacings at the edges when switching to full screen
+autocmd("VimEnter", {
+  command = ":silent !wezterm cli set-padding --left 0 --right 0 --top 0 --bottom 0",
+})
+
+autocmd("VimLeavePre", {
+  command = ":silent !wezterm cli set-padding --left 20 --right 20 --top 10 --bottom 10",
 })
